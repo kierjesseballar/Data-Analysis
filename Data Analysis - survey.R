2c57be3968a1f48447b3e52949a5c668119681b0
Kier Jesse Ballar <kierballar55@gmail.com>
2026-01-13 10:40:44 +0800

    added new models and cleaned old code

--- Data Analysis - survey.R
+++ Data Analysis - survey.R
@@ -8,14 +8,12 @@ library(mice)
 library(tidyverse)
 filter <- dplyr::filter
 
-##### || Folders -----
+##### || Read Data -----
 sea_plm_folder <- "C:\\Users\\kdbal\\OneDrive\\Desktop\\WORK\\EWDG\\SEAPLM Reading\\SEA_PLM 2024\\SEA_PLM_MS24_DBs_2025.12.04_PHL-20251203T161126Z-1-001\\SEA_PLM_MS24_DBs_(2025.12.04)_PHL\\SPSS Data"
 
-##### || Raw Data for Reading -----
 ###### > SEAPLM Student -----
 file <- "SEA_PLM_MS24_PHL_ST_(2025.12.04).sav"
 file_name <- str_c(sea_plm_folder, file, sep = "\\")
-
 sea_student <- read_spss(file_name)
 
 dictionary_student <- data.frame(
@@ -34,30 +32,54 @@ dictionary_school <- data.frame(
   label = sapply(sea_school, function(x) attr(x, "label"))
 )
 
-##### || Analysis with Survey ------
+##### ||----- Analysis with Survey -----|| ------
+##### || Data and Survey Design Preparation -----
 ###### |> Make the urbanity variable -----
-facility_vars <- c("SC07B", "SC07C", "SC07D", "SC07E", "SC07J", "SC07K", "SC07M")
+facility_vars <- c(
+  "SC07B", # cinema 
+  "SC07C", # theater
+  "SC07D", # foreign lang schools
+  "SC07E", # museum
+  "SC07J", # shopping center
+  "SC07K", # market
+  "SC07M"  # hospital
+  )
+
 attr(sea_school$SC07B, "labels")
+attr(sea_school$SC06, "labels")
+str(sea_school$SC06)
 
-###### |> Make the urbanity variable -----
-attr(sea_student$SchLoc, "labels")
-
-sea_school_clean <- sea_school %>%
+sea_school_clean <- sea_school |> 
   # Recode SEA-PLM's 1=Yes and 2=No to 1=Yes/0=No
   mutate(across(all_of(facility_vars), ~ case_when(
     . == "1" ~ 1,
     . == "2" ~ 0,
-    TRUE ~ NA_real_ # Handle missing data safely
+    TRUE ~ NA_real_ 
   ))) %>%
   # Create the row-wise sum 
   rowwise() %>%
   mutate(urban_facility_index = sum(c_across(all_of(facility_vars)), na.rm = TRUE)) %>%
   ungroup() |> 
   select(-all_of(facility_vars)) |> 
-  # Create a synthetic urban/rural classification
+  # Combine the population variable with the facility 
+  # Use arbitrary threshold of 3, so if with 3 "urban" facilities, tagged as urbanlite
+  # chose 3 since markets are ubiquitious so default present
+  # PSA https://www.adb.org/sites/default/files/project-documents/51325/51325-001-tacr-en_5.pdf
+  # rural if fails to meet ANY of the following, at least 5k, establishments with 100 employees,
+  # five small establishments, at least five facilities from barangay hall
   mutate(urbanity_final = case_when(
-    SchLoc = 1 
+    SC06 == "1" ~ "Rural", # less than 3,000 people
+    SC06 == "2" & urban_facility_index < 3 ~ "Rural", # bet 3k to 15k
+    SC06 == "2" & urban_facility_index >= 3 ~ "Urban-like", # bet 3k to 15k
+    SC06 == "3" ~ "Urban",
+    SC06 == "4" ~ "Urban",
+    SC06 == "5" ~ "Urban",
+    T ~ NA
   ))
+
+test <- sea_school_clean |> 
+  select(SC06, urban_facility_index, urbanity_final) |> 
+  distinct()
 
 ###### |> Merge the student and the school data -----
 attr(sea_student$PL_PV1_R, "labels")
@@ -65,27 +87,47 @@ attr(sea_student$PL_PV1_R, "labels")
 school_student_merge <- sea_student |> 
   left_join(sea_school_clean) |> 
   as.data.frame() |> 
-  # mutate(across(where(is.labelled), as_factor)) |>
-  mutate(is_band2_1 = case_when(
+  # convert band labels into binary for logit, 1 = band 2 and 0 otherwise
+  mutate(is_band2_r_1 = case_when(
     PL_PV1_R <= 2 ~ 1,
     T ~ 0
   )) |> 
-  mutate(is_band2_2 = case_when(
+  mutate(is_band2_r_2 = case_when(
     PL_PV2_R <= 2 ~ 1,
     T ~ 0
   )) |> 
-  mutate(is_band2_3 = case_when(
+  mutate(is_band2_r_3 = case_when(
     PL_PV3_R <= 2 ~ 1,
     T ~ 0
   )) |> 
-  mutate(is_band2_4 = case_when(
+  mutate(is_band2_r_4 = case_when(
     PL_PV4_R <= 2 ~ 1,
     T ~ 0
   )) |> 
-  mutate(is_band2_5 = case_when(
+  mutate(is_band2_r_5 = case_when(
     PL_PV5_R <= 2 ~ 1,
     T ~ 0
-  ))
+  )) |> 
+  # Create single grade and multigrade id 
+  mutate(school_multigrade = case_when(
+    SC09A == "1" & SC09B == "2" ~ "Single-grade only",
+    SC09A == "2" & SC09B == "1" ~ "Multigrade only",
+    SC09A == "1" & SC09B == "1" ~ "Mixed (Single & Multi)",
+    SC09A == "2" & SC09B == "2" ~ "Neither/Other",
+    TRUE ~ NA_character_
+  )) |> 
+  mutate(school_multigrade = factor(school_multigrade,
+                levels = c("Single-grade only", "Multigrade only", 
+                           "Mixed (Single & Multi)", "Neither/Other"))
+  ) |> 
+  # relabel the multishift to be clearer
+  mutate(school_multishift = case_when(
+    SC09C == "1" ~ "multi-shift",
+    SC09C == "2" ~ "single-shift",
+  )) |> 
+  mutate(school_multishift = factor(school_multishift,
+                levels = c("single-shift", "multi-shift"))
+  )
 
 
 attr(sea_student$PL_PV1_R, "labels")
@@ -95,62 +137,143 @@ rep_colnames <- grep("rwgt", names(school_student_merge), value = TRUE)
 
 survey_design <- svrepdesign(
   data = school_student_merge,
-  type = "JKn",            
+  type = "JK2", # from manual
   weights = ~FWGT,          
   repweights = school_student_merge[, rep_colnames],
   combined.weights = TRUE, 
-  scale = 1,              
-  rscales = 1,
   ids = ~SchID, 
   stratum = ~StrtID  
 )
 
-##### || Model 1: No interactions -----
+##### || Model Setup -----
 length(unique(sea_student$SchID)) # only 156 schools
+predictors <- c(
+    "urbanity_final", # urban lite
+    "SchType", # School sector (private public)
+    "S_Age", # Age
+    "S_Gender", # Student gender
+    "S_SES", # Student SES index
+    "S_MISCED", # Highest educational level of mother
+    "S_FISCED", # Highest educational level of father
+    "S_ATTREAD", # Attitudes towards reading
+    "ST13", # Attend preschool, note reverse scale with 1 = as highest
+    "ST14", # Repeated a grade, note 1 = yes
+    "C_RATIO", # Ratio of students to teachers
+    # School - Individual classes are organised according to the student’s year level
+    # School - Individual classes have students with multiple year levels
+    "school_multigrade", 
+    "school_multishift", # School - Classes are organised into shifts, where not all students are at school at the same time
+    "SC10", # On a typical school day, approximately how many hours do students receive lessons in the school?
+    "SC11" # In a typical school year, approximately how many instructional weeks do students receive lessons in the school?
+    )
+
+# Check Attributes
+for (var in predictors) {
+  val_labs <- attr(school_student_merge[[var]], "labels")
+  cat("---", var, "---\n")
+  if (!is.null(val_labs)) {
+    print(val_labs)
+  } else {
+    cat("No value labels found (likely a continuous variable).\n")
+  }
+  cat("\n")
+}
+
+# Register Formula
+model_formula <- reformulate(response = "reading_score", termlabels = predictors)
+
+##### || Model 1: Base Model, no interactions, no other variables -----
+# Run logit model
+# copy formula code for direct type, does not work if formula = model_formula idk why
+model_formula
 
 model_1_pvs <- withPV(
-  mapping = list(reading_score ~ is_band2_1 + is_band2_2 + is_band2_3 + is_band2_4 + is_band2_5),
-  data = survey_design,
-  action = quote(svyglm(reading_score ~ urban_facility_index + SchLoc + SchType + S_Age + S_Gender + S_SES + S_PARED + ST13 + C_RATIO + SC09A + SC09B + SC09C + SC10 + SC11, 
-                        family = quasibinomial(),
-                        design = survey_design)
+    mapping = list(
+                  reading_score ~ is_band2_r_1 + is_band2_r_2 + 
+                    is_band2_r_3 + is_band2_r_4 + is_band2_r_5
                 ),
+    data = survey_design,
+    action = quote(
+                svyglm(
+                      reading_score ~ urbanity_final + SchType + S_Age + S_Gender + 
+                        # S_SES + 
+                        S_MISCED + S_FISCED + S_ATTREAD + ST13 + ST14 + C_RATIO + 
+                        school_multigrade + school_multishift + SC10 + SC11, 
+                      family = quasibinomial(), design = survey_design)
+              ),
   rewrite = TRUE
 )
 
+# Pool the coefficients and se
 model_1_pooled <- MIcombine(model_1_pvs)
 
-summary(model_1_pooled)
-exp(coef(model_1_pooled))
+# Examine the results 
+results <- summary(model_1_pooled) |> 
+  mutate(across(.cols = -missInfo, 
+                ~exp(.))) |> 
+  rownames_to_column()
 
-t_stats <- coef(model_1_pooled) / SE(model_1_pooled)
-p_vals <- 2 * pt(-abs(t_stats), df = model_1_pooled$df)
-p_vals 
+p_vals <- (2 * pt(-abs(coef(model_1_pooled) / SE(model_1_pooled)), df = model_1_pooled$df)) |> 
+  as.data.frame() |> 
+  rename(p_value = 1) |> 
+  rownames_to_column()
+
+model_1_results <- results |> 
+  left_join(p_vals) |> 
+  mutate(sig = case_when(
+    p_value < 0.001 ~ "***",
+    p_value < 0.01 ~ "**",
+    p_value < 0.05 ~ "*",
+    p_value < 0.1 ~ ".",
+    TRUE ~ "" 
+  ))
 
 
+##### || Model 2: Base model with interactions -----
 
+##### || Model 3: Base model with interactions + other variables -----
+# Run logit model
+# copy formula code for direct type, does not work if formula = model_formula idk why
+model_formula
 
-full_mira <- as.mira(model_1_pvs)
-predictors <- names(coef(model_1_pvs[[1]]))[-1] 
-omnibus_list <- lapply(predictors, function(var) {
-  
-  # Use lapply to update each model in the list
-  model_reduced_list <- lapply(model_1_pvs, function(m) {
-    # We use update with a rigorous formula environment
-    update(m, as.formula(paste(". ~ . -", var)))
-  })
-  
-  # Convert reduced list to mira object
-  reduced_mira <- as.mira(model_reduced_list)
-  
-  # Perform D1 test - wrapping in try() handles the NA replicate instability
-  test <- tryCatch({
-    D1(full_mira, reduced_mira)
-  }, error = function(e) {
-    return(paste("Error in", var, ":", e$message))
-  })
-  
-  return(test)
-})
+model_1_pvs <- withPV(
+    mapping = list(
+                  reading_score ~ is_band2_r_1 + is_band2_r_2 + 
+                    is_band2_r_3 + is_band2_r_4 + is_band2_r_5
+                ),
+    data = survey_design,
+    action = quote(
+                svyglm(
+                      reading_score ~ urbanity_final + SchType + S_Age + S_Gender + 
+                        # S_SES + 
+                        S_MISCED + S_FISCED + S_ATTREAD + ST13 + ST14 + C_RATIO + 
+                        school_multigrade + school_multishift + SC10 + SC11, 
+                      family = quasibinomial(), design = survey_design)
+              ),
+  rewrite = TRUE
+)
 
-names(omnibus_list) <- predictors
+# Pool the coefficients and se
+model_1_pooled <- MIcombine(model_1_pvs)
+
+# Examine the results 
+results <- summary(model_1_pooled) |> 
+  mutate(across(.cols = -missInfo, 
+                ~exp(.))) |> 
+  rownames_to_column()
+
+p_vals <- (2 * pt(-abs(coef(model_1_pooled) / SE(model_1_pooled)), df = model_1_pooled$df)) |> 
+  as.data.frame() |> 
+  rename(p_value = 1) |> 
+  rownames_to_column()
+
+model_1_results <- results |> 
+  left_join(p_vals) |> 
+  mutate(sig = case_when(
+    p_value < 0.001 ~ "***",
+    p_value < 0.01 ~ "**",
+    p_value < 0.05 ~ "*",
+    p_value < 0.1 ~ ".",
+    TRUE ~ "" 
+  ))
+
